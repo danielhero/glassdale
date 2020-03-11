36ff71808ab62ebcb60dfffb76835be8cce4f69e
danielhero <danielhero@outlook.com>
2020-03-11 13:54:05 -0500

    added filter selector for crimelist

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -3,6 +3,33 @@ import { Criminal } from "./CriminalHTML.js";
 
 
 const contentTarget = document.querySelector(".criminalsContainer")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("crimeChosen", event => {
+    // Filter the list of criminal who committed the crime
+
+    // Get the criminals
+    const criminals = useCriminals()
+
+    // Get the crime
+    const theCrimeThatWasChosen = event.detail.chosenCrime
+
+    // Look at all of the criminals and determine if each one is a vandal
+    const guiltyCriminals = criminals.filter(criminal => {
+        if (criminal.conviction === theCrimeThatWasChosen) {
+            return true
+        }
+        return false
+    })
+
+     // Clear inner HTML of the criminal list
+     contentTarget.innerHTML = ""
+
+     // Build it up again
+     for (const singleCriminal of guiltyCriminals) {
+         contentTarget.innerHTML += Criminal(singleCriminal)
+     }
+ })
 
 
 
